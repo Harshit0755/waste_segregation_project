8b9c6294835d7152b7bc5daeae4dcfd75a317eab
Harshit Arora <64795664+Harshit0755@users.noreply.github.com>
2021-01-06 23:33:02 +0530

    first commit

--- gitbot.py
+++ gitbot.py
@@ -12,4 +12,4 @@ for i in range(1,2*2 + 1):
     os.system('git commit --date="' + d + '" -m "first commit"')
 
 ## push the commit to github
-os.system('git push origin master')
+os.system('git push origin BRANCH')
